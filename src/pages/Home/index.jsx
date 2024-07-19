2916845822d74b44253979981baf0eb8aeb85948
Charles-cdel <lionbllack31@gmail.com>
2024-07-19 16:28:20 -0300

    adicionei productCard

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,16 @@
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import Main from "../../components/Main";
+import ProductCard from "../../components/ProductCard";
 
 function Home() {
     return ( 
         <>
             <Header/>
             <Main/>
+            <ProductCard/>
             <Footer/>
+            
         </>
      );
 }
