a10d8147e51c00a5c18001a6fcff22b7c78ba457
Carlosgit <lionbllack31@gmail.com>
2024-07-22 16:22:46 -0300

    atualizando

--- src/components/RoadFeet/index.jsx
+++ src/components/RoadFeet/index.jsx
@@ -3,30 +3,16 @@ import BigShoe from'../../assets/BigShoe.png'
 function RoadFeet(){
   return(
     <div className="flex content-center items-center w-[90rem] h-[34.57rem] border-solid border-2 border-red-700">
-      <div className="w-[40%] h-[34rem] border-solid border-2 border-blue-700">
-        <div className="w-[35.82rem] h-[20.65rem] border-solid border-2 border-yellow-500">
-          <img className='' src={BigShoe} alt="" />
+      <div className="w-[40%]  h-[34rem] ml-16 border-solid border-2 border-blue-700">
+        <div className="w-[35.82rem] flex h-[20.65rem] border-solid ml-16 mt-24 border-2 border-yellow-500">
+          <div className=" border-solid border-2 border-pink-700 rounded-xl ">
+          <img className=' shadow-2xl rouded-[20rem]' src={BigShoe} alt="" />
+          </div>
+          
         
 
         </div>
-         <div className="w-[36.82rem] h-[21.32rem]">
-          <div className="">
-            <p className=''>
-
-            </p>
-            <div className="">
-              <h1 class=''>
-
-              </h1>
-            </div>
-          </div>
-          <div className="">
-            <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt officia quaerat ipsum voluptatibus cum excepturi sed! Maiores consequatur autem expedita, error, neque saepe aperiam distinctio hic doloremque vel sint nihil.</p>
-          </div>
-         </div>
-         <div className="">
-          <button className=''> Ver Ofertas</button>
-         </div>
+       
       </div>
     </div>
   )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,14 +3,11 @@ import StripProduct from"../../components/StripProduct"
 import Header from "../../components/Header";
 import Main from "../../components/Main";
 import ProductCard from "../../components/ProductCard";
-<<<<<<< HEAD
 import ProductBar from "../../components/ProductBar";
 import Produtos from "../../components/Api";
 import RoadFeet from "../../components/RoadFeet";
 
 
-=======
->>>>>>> 44e6382eb62111a6e5cc61a80ef9f0bd9c114293
 
 function Home() {
     return ( 
@@ -18,14 +15,11 @@ function Home() {
             <Header/>
             <Main/>
             <ProductCard/>
-<<<<<<< HEAD
             <StripProduct/>
             <ProductBar/>
             <Produtos/>
             <RoadFeet/>
-=======
->>>>>>> 44e6382eb62111a6e5cc61a80ef9f0bd9c114293
-            <Footer/>
+                        <Footer/>
             
         </>
      );
