d0d4db9959bb3ed123c209d941cc9b5895d0832b
Carlosgit <lionbllack31@gmail.com>
2024-07-23 15:51:08 -0300

    atualizando api

--- src/components/Api/index.jsx
+++ src/components/Api/index.jsx
@@ -7,7 +7,7 @@ function Produtos (){
 
   
   useEffect(() => {
-    instance.get('/shoes?limit=1')
+    instance.get('/shoes?limit=8')
         .then((response) => {
             setProdutos(response.data)
             console.log(response.data)
@@ -17,124 +17,27 @@ function Produtos (){
     
   
   return(
-     <div className=" w-[90rem] flex boder-solid border-2 border-orange-500">
-      <div className="justify-items-center flex-wrap boder-solid border-2 border-black gap[0.75rem]">
-      {produtos.map(produto=>(
-        <div className="" key={produto.id}>
-           
-           <div className="w-[6rem] h-[2rem]  rounded-[10px] flex items-center justify-center gap-1 bg-[#E7FF86] ml-4 mt-2">
-              <h5 className="font-bold text-sm tracking-widest text-[#474747]">
-                {' '}
-                30% OFF{' '}
-              </h5>
-            </div>
-            <div className="w-[18.25rem] flex h-[20.063rem] border-solid border-2 shadow-2xl border-yellow-500">
-            <div className="w-[15.5rem] flex content-center items-center h-[8.4rem] border-solid border-2 border-red-600">
-             <img className='w-[10rem] ml-5 h-[8rem]' src={produto.imagem_url} alt="" />
-             
-             </div>
-               </div>
-           
-             
-            
-            
-           <p > {produto.nome}</p>
-           <p>{produto.preco_original}</p>
+     <div className=" flex flex-wrap justify-center boder-solid border-2 border-orange-500">
+      {produtos.map ((produto) => (
+        <div className="flex w-[15rem] h-[30rem] border-solid border-2 border-red-700 " key={produto.id}>
 
-          <div className="w-[6rem] h-[2rem]  rounded-[10px] flex items-center justify-center gap-1 bg-[#E7FF86] ml-4 mt-2">
-              <h5 className="font-bold text-sm tracking-widest text-[#474747]">
-                {' '}
-                30% OFF{' '}
-              </h5>
-            </div>
-           <div className="w-[18.25rem]flex h-[20.063rem] border-solid border-2 shadow-2xl border-yellow-500">
-            <div className="w-[15.5rem] flex content-center items-center h-[8.4rem] border-solid border-2 border-red-600">
-             <img className='w-[10rem] ml-5 h-[8rem]' src={produto.imagem_url} alt="" />
-             
-             </div>
-               </div>
-           
-             
-            
-            
-           <p > {produto.nome}</p>
-           <p>{produto.preco_original}</p>
-           <div className="w-[18.25rem] h-[20.063rem] border-solid border-2 shadow-2xl border-yellow-500">
-            <div className="w-[15.5rem] flex content-center items-center h-[8.4rem] border-solid border-2 border-red-600">
-             <img className='w-[10rem] ml-5 h-[8rem]' src={produto.imagem_url} alt="" />
-             
-             </div>
-               </div>
-           
-             
-            
-            
-           <p > {produto.nome}</p>
-           <p>{produto.preco_original}</p>
-           <div className="w-[18.25rem] h-[20.063rem] border-solid border-2 shadow-2xl border-yellow-500">
-            <div className="w-[15.5rem] flex content-center items-center h-[8.4rem] border-solid border-2 border-red-600">
-             <img className='w-[10rem] ml-5 h-[8rem]' src={produto.imagem_url} alt="" />
-             
-             </div>
-               </div>
-           
-             
-            
-            
-           <p > {produto.nome}</p>
-           <p>{produto.preco_original}</p>
-           <div className="w-[18.25rem] h-[20.063rem] border-solid border-2 shadow-2xl border-yellow-500">
-            <div className="w-[15.5rem] flex content-center items-center h-[8.4rem] border-solid border-2 border-red-600">
-             <img className='w-[10rem] ml-5 h-[8rem]' src={produto.imagem_url} alt="" />
-             
-             </div>
-               </div>
-           
-             
-            
-            
-           <p > {produto.nome}</p>
-           <p>{produto.preco_original}</p>
-           <div className="w-[18.25rem] h-[20.063rem] border-solid border-2 shadow-2xl border-yellow-500">
-            <div className="w-[15.5rem] flex content-center items-center h-[8.4rem] border-solid border-2 border-red-600">
-             <img className='w-[10rem] ml-5 h-[8rem]' src={produto.imagem_url} alt="" />
-             
-             </div>
-               </div>
-           
-             
-            
-            
-           <p > {produto.nome}</p>
-           <p>{produto.preco_original}</p>
-           <div className="w-[18.25rem] h-[20.063rem] border-solid border-2 shadow-2xl border-yellow-500">
-            <div className="w-[15.5rem] flex content-center items-center h-[8.4rem] border-solid border-2 border-red-600">
-             <img className='w-[10rem] ml-5 h-[8rem]' src={produto.imagem_url} alt="" />
-             
-             </div>
-               </div>
-           
-             
-            
-            
-           <p > {produto.nome}</p>
-           <p>{produto.preco_original}</p>
-           <div className="w-[18.25rem] h-[20.063rem] border-solid border-2 shadow-2xl border-yellow-500">
-            <div className="w-[15.5rem] flex content-center items-center h-[8.4rem] border-solid border-2 border-red-600">
-             <img className='w-[10rem] ml-5 h-[8rem]' src={produto.imagem_url} alt="" />
-             
-             </div>
-               </div>
-           
-             
-            
-            
-           <p > {produto.nome}</p>
-           <p>{produto.preco_original}</p>
+          <div className="bg-white w-full h-[13.8rem] ">
+          <img className='w-[15rem] self-center' src={produto.imagem_url} alt={produto.nome} />
+          </div>
+          <div className="">
+
+           <p>tenis</p>
+           <h1>{produto.nome}</h1>
+           <div className="flex">
+            <p>
+              R$ {produto.preco_original},00
+            </p>
+            <p>R${produto.preco_desconto},00</p>
+           </div>
+          </div>
+
         </div>
-        
-      ))}
-    </div>
+      ) )}
    
     </div>
    )}
